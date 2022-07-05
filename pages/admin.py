2eaafd1bbd1e6ec8641afd9c6f5c1b40c5f11d37
JakSam <JakSam79@gmail.com>
2022-07-05 14:53:20 +0300

    Мелкие правки

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from pages.models import Project, BankTransferInfo, ConsituentsDocs, Addresses
 
 admin.site.register(Project, admin.ModelAdmin)
-admin.site.register(BankTransferInfo, admin.ModelAdmin)
+
+
+class BankTransferInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'currency', 'project')
+    list_filter = ('currency', 'project')
+
+admin.site.register(BankTransferInfo, BankTransferInfoAdmin)
+
 admin.site.register(ConsituentsDocs, admin.ModelAdmin)
 admin.site.register(Addresses, admin.ModelAdmin)
